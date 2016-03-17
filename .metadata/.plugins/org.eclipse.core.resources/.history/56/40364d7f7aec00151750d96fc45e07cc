c647cc36319629414795df122398b0c81410cd0b
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-03-17 14:01:51 -0600

    Added gitignore

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/56/40364d7f7aec00151750d96fc45e07cc
@@ -0,0 +1,16 @@
+'''
+Created on Mar 17, 2016
+
+@author: Jani
+'''
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
--- bayesnets/readbif/node.py
+++ bayesnets/readbif/node.py
@@ -3,14 +3,18 @@ Created on Mar 17, 2016
 
 @author: Jani
 '''
-class MyClass(object):
+class Node(object):
     '''
-    classdocs
+    Each Node stores a node name, a set of states, a conditional probability table and the parents given in table
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, name, states, table, tablegivens ):
         '''
         Constructor
         '''
+        self.name = name; #node name
+        self.states = states; #tuple of states
+        self.table = table; #dictionary of tuples
+        self.tablegivens = tablegivens; #tuple of nodes from table
         
