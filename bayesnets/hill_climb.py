1af6f71f95dcd9694a1af69bdd9b299c12afa936
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-04-04 16:38:39 -0600

    Expanded the probability table recalculation

--- bayesnets/hill_climb.py
+++ bayesnets/hill_climb.py
@@ -9,7 +9,7 @@ def run():
     varlist = list(tables.keys()) #determine the list of variables
     tablist = list(tables.values())
     #print(varlist)
-    #print(tables['KINKEDTUBE'].getTable())
+    print(tables['HYPOVOLEMIA'].getTableGivens())
     edges = list()
     edgesprime = list()
     score = -maxsize
@@ -18,10 +18,12 @@ def run():
     #while(score > maxscore):
     maxscore = score
     for x in tables:
+        print(x, len(tables[x].getTableGivens()))
         for y in tables:
             if x != y:
                 taby = tables[y].getTable()
                 tabx = tables[x].getTable()
+                
                 if(x,y) in edges or (y,x) in edges:
                     #print((x,y))
                     #subtract edge
@@ -38,31 +40,84 @@ def run():
                 if newscore > score:
                     score = newscore
                         
-def bicScore(edgeList, probTables):
+def bicScore(edgeList, tablesPrime):
     return 0
 
 def recalculateProbTables(edgesList, probTables, tablesPrime):
-    print('recalculating...')
+    #print('recalculating...')
+    tablesPrime = OrderedDict()
     for (x,y) in edgesList:
-        z = probTables[x].getTableGivens()
-        if y in z:
-            i = z.index(y)
-            olist = probTables[x].getTable()
-            statelist = probTables[y].getStates()
+        z = probTables[y].getTableGivens()
+        yStates = probTables[y].getStates()
+        yProb = probTables[y].getTable()
+        substatelist = probTables[x].getStates()
+        if x in z:
+            if len(z) == 1:
+                tablesPrime[y] = bif_parser.Node(y, yStates, z, yProb)
+                #print(y, tablesPrime[y])
+            elif len(z) >1:
+                count  = 0
+                for i in z:
+                    if (i, y) in edgesList:
+                        count += 1
+                    else:
+                        #want to remove i from z and i from table
+                        iplace = z.index(i)
+                        z.remove(i)
+                        #print(y, len(yProb), yProb)
+                        #should cycle through yprob,
+                        for line in yProb:
+                            #create partial tuples of yprob key with None instead of i states
+                            tuplelen = len(line)
+                            a = list(line)
+                            a[iplace] = None
+                            a = tuple(a)
+                            #Find all partial tuples that match each other
+                            tuplesmatch = GetTuples(yProb, a, tuplelen)
+                            print(tuplesmatch)
+                            a = list(a)
+                            a.pop(iplace)
+                            a = tuple(a)
+                            #add the probabilities of the matching tuples
+                            probability = []
+                            for state in yStates:
+                            
+                            #Divide each probability by the number of matching partial tuples
+
+                            #create a dictionary of a:probability
+                if count == len(z):
+                    tablesPrime[y] = bif_parser.Node(y, yStates, z, yProb)
+                #i = z.index(y)
+           
             
-            print(olist)
+            #print(olist)
             #find the probability in the table
             #calculate
         else:
+            if not z[0]:
+               # print(z)
+                #print(len(z))
+                pass
+            else:
+                pass
             #print(z)
-            pass
+            #pass
             #calculate the probability and create the table
     
         
     return probTables
                         
-                
-                
+def GetTuples(probTable, keyWords, tuplelen):
+    tuples = []
+    for k in probTable.keys():
+        match = True
+        for i in range(tuplelen):
+            if keyWords[i] is not None and keyWords[i] != k[i]:
+                match = False
+                break
+        if match is True:
+            tuples.append(k)
+    return tuples      
                     
     
 
