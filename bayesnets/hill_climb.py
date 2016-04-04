bc7c5cf081ef23f55f4ffa116f587d40adb8e800
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-04-03 23:01:04 -0600

    More fiddling with hill-climb

--- bayesnets/hill_climb.py
+++ bayesnets/hill_climb.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 def run():
     m = bif_parser.Parser() #instantiate parser
     tables = m.createTable('alarm.bif') #call parsing function
+    tablesprime = OrderedDict()
     varlist = list(tables.keys()) #determine the list of variables
     tablist = list(tables.values())
     #print(varlist)
@@ -13,31 +14,51 @@ def run():
     edgesprime = list()
     score = -maxsize
     maxscore = score -1
-    while(score > maxscore):
-        maxscore = score
-        for x in tables:
-            for y in tables:
-                if x != y:
-                    taby = tables[y].getTable()
-                    tabx = tables[x].getTable()
-                    if(x,y) in edges:
-                        print((x,y))
-                        #subtract edge
-                        #recalculate probability tables and score
-                        #flip edge
-                        #recalculate probability tables and score
-                        newscore = 0
-                    else:
-                        print(x,y)
-                        #add edge
-                        #recalculate probability tables and score
-                    if newscore > score:
-                        score = newscore
+    newscore = maxscore
+    #while(score > maxscore):
+    maxscore = score
+    for x in tables:
+        for y in tables:
+            if x != y:
+                taby = tables[y].getTable()
+                tabx = tables[x].getTable()
+                if(x,y) in edges or (y,x) in edges:
+                    #print((x,y))
+                    #subtract edge
+                    #recalculate probability tables and score
+                    #flip edge
+                    #recalculate probability tables and score
+                    newscore = 0
+                else:
+                    #print(x,y)
+                    #add edge
+                    edgesprime.append((x,y))
+                    tablesPrime = recalculateProbTables(edgesprime, tables, tablesprime)
+                    #recalculate probability tables and score
+                if newscore > score:
+                    score = newscore
                         
-def bifScore(edgeList, probTables):
+def bicScore(edgeList, probTables):
     return 0
 
-def recalculateProbTables(edgesList, probTables):
+def recalculateProbTables(edgesList, probTables, tablesPrime):
+    print('recalculating...')
+    for (x,y) in edgesList:
+        z = probTables[x].getTableGivens()
+        if y in z:
+            i = z.index(y)
+            olist = probTables[x].getTable()
+            statelist = probTables[y].getStates()
+            
+            print(olist)
+            #find the probability in the table
+            #calculate
+        else:
+            #print(z)
+            pass
+            #calculate the probability and create the table
+    
+        
     return probTables
                         
                 
