70458f4fcbb2819f7e3add71b30472ad33a09e18
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-04-17 17:16:45 -0600

    finished writing base hill-climb. still need to write BICscore and recalculate probability tables

--- bayesnets/hill_climb.py
+++ bayesnets/hill_climb.py
@@ -5,103 +5,60 @@ from collections import OrderedDict
 def run():
     m = bif_parser.Parser() #instantiate parser
     tables = m.createTable('alarm.bif') #call parsing function
-    tablesprime = OrderedDict()
+    tablesprime = OrderedDict() #creat probability tables list
     varlist = list(tables.keys()) #determine the list of variables
     tablist = list(tables.values())
-    #print(varlist)
-    print(tables['HYPOVOLEMIA'].getTableGivens())
-    edges = list()
+    edges = list() #create edge list
     edgesprime = list()
-    score = -maxsize
+    score = -maxsize #set score to large negative value
     maxscore = score -1
     newscore = maxscore
-    #while(score > maxscore):
-    maxscore = score
-    for x in tables:
-        #print(x, len(tables[x].getTableGivens()))
-        for y in tables:
-            if x != y:
-                taby = tables[y].getTable()
-                tabx = tables[x].getTable()
+    while(score > maxscore):
+        maxscore = score
+        for x in tables:
+            #print(x, len(tables[x].getTableGivens()))
+            for y in tables:
+                if x != y:
+                    taby = tables[y].getTable()
+                    tabx = tables[x].getTable()
                 
-                if(x,y) in edges or (y,x) in edges:
-                    #print((x,y))
-                    #subtract edge
-                    #recalculate probability tables and score
-                    #flip edge
-                    #recalculate probability tables and score
-                    newscore = 0
-                else:
-                    #print(x,y)
-                    #add edge
-                    edgesprime.append((x,y))
-                    tablesPrime = recalculateProbTables(edgesprime, tables, tablesprime)
-                    #recalculate probability tables and score
-                if newscore > score:
-                    score = newscore
+                    if(x,y) in edges:
+                        #print((x,y))
+                        #subtract edge
+                        edgesprime.remove((x,y))
+                        #recalculate probability tables and score
+                        tablesPrime = recalculateProbTables(edgesprime, tables, tablesprime)
+                        newscore = bicScore(edgesPrime, tablesPrime)
+                        if newscore > score:
+                            edges = edgesprime.deepcopy()
+                            score = newscore
+                            
+                        #flip edge
+                        edgesprime.remove((x,y))
+                        edgesprime.append((y,x))
+                        #recalculate probability tables and score
+                        tablesPrime = recalculateProbTables(edgesprime, tables, tablesprime)
+                        newscore = bicScore(edgesPrime, tablesPrime)
+                        if newscore > score:
+                            edges = edgesprime.deepcopy()
+                            score = newscore
+                    else:
+                        #print(x,y)
+                        #add edge
+                        edgesprime.append((x,y))
+                        #recalculate probability tables and score
+                        tablesPrime = recalculateProbTables(edgesprime, tables, tablesprime)
+                        newscore = bicScore(edgesPrime, tablesPrime)
+                        if newscore > score:
+                            edges = edgesprime.deepcopy()
+                            score = newscore
+                        
                         
 def bicScore(edgeList, tablesPrime):
     return 0
 
 def recalculateProbTables(edgesList, probTables, tablesPrime):
-    #print('recalculating...')
-    tablesPrime = OrderedDict()
-    for (x,y) in edgesList:
-        z = probTables[y].getTableGivens()
-        yStates = probTables[y].getStates()
-        yProb = probTables[y].getTable()
-        substatelist = probTables[x].getStates()
-        if x in z:
-            if len(z) == 1:
-                tablesPrime[y] = bif_parser.Node(y, yStates, z, yProb)
-            elif len(z) >1:
-                count  = 0
-                probTablePrime = OrderedDict()
-                for i in z:
-                    if (i, y) in edgesList:
-                        count += 1
-                    else:
-                        #want to remove i from z and i from table
-                        iplace = z.index(i)
-                        z.remove(i)
-                        #should cycle through yprob,
-                        for line in yProb:
-                            #create partial tuples of yprob key with None instead of i states
-                            tuplelen = len(line)
-                            a = list(line)
-                            a[iplace] = None
-                            a = tuple(a)
-                            #Find all partial tuples that match each other
-                            tuplesmatch = GetTuples(yProb, a, tuplelen)
-                            a = list(a)
-                            a.pop(iplace)
-                            a = tuple(a)
-                            probability = []
-                            for state in range(len(yStates)):
-                                probSum = 0
-                                for match in tuplesmatch:
-                                    #add the probabilities of the matching tuples
-                                    probSum += yProb[match][state]
-                                #Divide each probability by the number of matching partial tuples
-                                probability.append(probSum / len(tuplesmatch))
-                            #create a dictionary entry of a:probability
-                            probTablePrime[a] = probability
-                if count == len(z):
-                    tablesPrime[y] = bif_parser.Node(y, yStates, z, yProb)
-                else:
-                    tablesPrime[y] = bif_parser.Node(y, yStates, z, probTablePrime)
-        else:
-            if not z[0]:
-               # print(z)
-                #print(len(z))
-                pass
-            else:
-                pass
-            #print(z)
-            #pass
-            #calculate the probability and create the table
-    
-        
+   
     return probTables
                         
 def GetTuples(probTable, keyWords, tuplelen):
